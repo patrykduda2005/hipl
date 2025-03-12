db7122eb07c8e8ebfd7b4c6703ba3b7d8cda665b
patrykduda2005 <patrykduda7@gmail.com>
2025-03-12 08:08:02 +0100

    moved FS functions out of table

--- main.lua
+++ main.lua
@@ -3,131 +3,138 @@ CONFIG = {
     dist_folder = "./dist",
 }
 
+--FS
 FS = {
     LSOPTION = {
         FOLDERS = {},
         FILES = {},
     },
-    getFileName = function(filePath)
-        local fileNameOffset = filePath:find("/[^/]*$")
-        if fileNameOffset ~= nil then
-            fileNameOffset = fileNameOffset + 1
-        else
-            fileNameOffset = 0
-        end
-        local output = filePath:sub(fileNameOffset)
-        if output == "" then return nil
-        else return output
-        end
-    end,
-    getFolderPath = function(filePath)
-        return filePath:gsub("/[^/]*$", "") .. '/'
-    end,
-    getPathBuilder = function(filepath)
-        if filepath == nil then return nil end
-        local pathBuilder = {}
-        for folderName in string.gmatch(FS.getFolderPath(filepath), "[^/]*/") do
-            table.insert(pathBuilder, folderName)
-        end
-        local filename = FS.getFileName(filepath)
-        if filename ~= nil then
-            table.insert(pathBuilder, filename)
-        end
-        return pathBuilder
-    end,
-    convertToRelativePath = function(absoluteFilePath, relativeToFolderPath)
-        if absoluteFilePath == nil or relativeToFolderPath == nil then return nil end
-        local pathBuilder = {}
-        local afpPathBuilder = FS.getPathBuilder(FS.getFolderPath(absoluteFilePath))
-        local rtfpPathBuilder = FS.getPathBuilder(FS.getFolderPath(relativeToFolderPath))
-        local offsetToAppendTheRest = nil
-        for k, v in pairs(rtfpPathBuilder) do
-            if v == afpPathBuilder[k] then
-                offsetToAppendTheRest = k + 1
-            else
-                table.insert(pathBuilder, "../")
-            end
-        end
-        for i = offsetToAppendTheRest, #afpPathBuilder, 1 do
-            table.insert(pathBuilder, afpPathBuilder[i])
-        end
-
-        table.insert(pathBuilder, FS.getFileName(absoluteFilePath))
-
-        local output = ""
-        for _, folderName in pairs(pathBuilder) do
-            output = output .. folderName
-        end
-
-        return output
-    end,
-    concatPaths = function(lhs, rhs)
-        if lhs == '' then return rhs end
-        if lhs:find("%.%.") ~= nil then return nil end
-        --simple concat
-        if rhs:find("%.%.") == nil then
-            if lhs:sub(-1) == '/' then
-                return lhs .. rhs
-            end
-            return lhs .. '/' .. rhs
-        end
-
-        --complex concat
-        local pathBuilder = {}
-        for folderName in lhs:gmatch("[^/]*/") do
-            table.insert(pathBuilder, folderName)
-        end
-        for folderName in rhs:gmatch("[^/]*/") do
-            if folderName == "../" then
-                table.remove(pathBuilder)
-            else
-                table.insert(pathBuilder, folderName)
-            end
-        end
-        local rhsFileName = FS.getFileName(rhs)
-        if rhsFileName == nil then return nil end
-        table.insert(pathBuilder, FS.getFileName(rhs))
-        local output = ""
-        for _, folderName in pairs(pathBuilder) do
-            output = output .. folderName
-        end
-        return output
-    end,
-    mkdir = function(folderPath)
-        local mkdirUnix = string.format("mkdir -p %s", folderPath)
-        local folderPathWindows = folderPath:gsub("/", "\\")
-        local mkdirWindows = string.format("mkdir%s 2>/dev/null", folderPathWindows)
-        print("MKDIR COMMAND: ", mkdirUnix)
-        local command = io.popen(string.format("%s || %s", mkdirWindows, mkdirUnix))
-        return command
-    end,
-    ls = function(type, path)
-        local output = {}
-        local command_output = nil
-
-        if type == FS.LSOPTION.FILES then
-            local findFilesWindows = string.format("dir/a:-d /b %s 2>/dev/null", path)
-            local findFilesUnix = string.format("ls -p %s | grep -v /", path)
-            command_output = io.popen(string.format("%s || %s", findFilesWindows, findFilesUnix))
-        elseif type == FS.LSOPTION.FOLDERS then
-            local findFoldersWindows = string.format("dir/a:d /b %s 2>/dev/null", path)
-            local findFoldersUnix = string.format("ls -p %s | grep /", path)
-            command_output = io.popen(string.format("%s || %s", findFoldersWindows, findFoldersUnix))
-        end
-
-        if command_output == nil then return nil end
-        for folderOrFilename in command_output:lines() do
-            if type == FS.LSOPTION.FOLDERS and folderOrFilename:sub(-1) ~= '/' then --unix has slash at the end, but windows does not 
-                folderOrFilename = folderOrFilename .. "/"
-            end
-            table.insert(output, folderOrFilename)
-        end
-        command_output:close()
-        return output
-    end,
 }
 
+function FS.getFileName(filePath)
+    local fileNameOffset = filePath:find("/[^/]*$")
+    if fileNameOffset ~= nil then
+        fileNameOffset = fileNameOffset + 1
+    else
+        fileNameOffset = 0
+    end
+    local output = filePath:sub(fileNameOffset)
+    if output == "" then return nil
+    else return output
+    end
+end
 
+function FS.getFolderPath(filePath)
+    return filePath:gsub("/[^/]*$", "") .. '/'
+end
+function FS.getPathBuilder(filepath)
+    if filepath == nil then return nil end
+    local pathBuilder = {}
+    for folderName in string.gmatch(FS.getFolderPath(filepath), "[^/]*/") do
+        table.insert(pathBuilder, folderName)
+    end
+    local filename = FS.getFileName(filepath)
+    if filename ~= nil then
+        table.insert(pathBuilder, filename)
+    end
+    return pathBuilder
+end
+
+function FS.convertToRelativePath(absoluteFilePath, relativeToFolderPath)
+    if absoluteFilePath == nil or relativeToFolderPath == nil then return nil end
+    local pathBuilder = {}
+    local afpPathBuilder = FS.getPathBuilder(FS.getFolderPath(absoluteFilePath))
+    local rtfpPathBuilder = FS.getPathBuilder(FS.getFolderPath(relativeToFolderPath))
+    local offsetToAppendTheRest = nil
+    for k, v in pairs(rtfpPathBuilder) do
+        if v == afpPathBuilder[k] then
+            offsetToAppendTheRest = k + 1
+        else
+            table.insert(pathBuilder, "../")
+        end
+    end
+    for i = offsetToAppendTheRest, #afpPathBuilder, 1 do
+        table.insert(pathBuilder, afpPathBuilder[i])
+    end
+
+    table.insert(pathBuilder, FS.getFileName(absoluteFilePath))
+
+    local output = ""
+    for _, folderName in pairs(pathBuilder) do
+        output = output .. folderName
+    end
+
+    return output
+end
+
+function FS.concatPaths(lhs, rhs)
+    if lhs == '' then return rhs end
+    if lhs:find("%.%.") ~= nil then return nil end
+    --simple concat
+    if rhs:find("%.%.") == nil then
+        if lhs:sub(-1) == '/' then
+            return lhs .. rhs
+        end
+        return lhs .. '/' .. rhs
+    end
+
+    --complex concat
+    local pathBuilder = {}
+    for folderName in lhs:gmatch("[^/]*/") do
+        table.insert(pathBuilder, folderName)
+    end
+    for folderName in rhs:gmatch("[^/]*/") do
+        if folderName == "../" then
+            table.remove(pathBuilder)
+        else
+            table.insert(pathBuilder, folderName)
+        end
+    end
+    local rhsFileName = FS.getFileName(rhs)
+    if rhsFileName == nil then return nil end
+    table.insert(pathBuilder, FS.getFileName(rhs))
+    local output = ""
+    for _, folderName in pairs(pathBuilder) do
+        output = output .. folderName
+    end
+    return output
+end
+
+function FS.mkdir(folderPath)
+    local mkdirUnix = string.format("mkdir -p %s", folderPath)
+    local folderPathWindows = folderPath:gsub("/", "\\")
+    local mkdirWindows = string.format("mkdir%s 2>/dev/null", folderPathWindows)
+    print("MKDIR COMMAND: ", mkdirUnix)
+    local command = io.popen(string.format("%s || %s", mkdirWindows, mkdirUnix))
+    return command
+end
+
+function FS.ls(type, path)
+    local output = {}
+    local command_output = nil
+
+    if type == FS.LSOPTION.FILES then
+        local findFilesWindows = string.format("dir/a:-d /b %s 2>/dev/null", path)
+        local findFilesUnix = string.format("ls -p %s | grep -v /", path)
+        command_output = io.popen(string.format("%s || %s", findFilesWindows, findFilesUnix))
+    elseif type == FS.LSOPTION.FOLDERS then
+        local findFoldersWindows = string.format("dir/a:d /b %s 2>/dev/null", path)
+        local findFoldersUnix = string.format("ls -p %s | grep /", path)
+        command_output = io.popen(string.format("%s || %s", findFoldersWindows, findFoldersUnix))
+    end
+
+    if command_output == nil then return nil end
+    for folderOrFilename in command_output:lines() do
+        if type == FS.LSOPTION.FOLDERS and folderOrFilename:sub(-1) ~= '/' then --unix has slash at the end, but windows does not 
+            folderOrFilename = folderOrFilename .. "/"
+        end
+        table.insert(output, folderOrFilename)
+    end
+    command_output:close()
+    return output
+end
+
+--FS end
 
 local hashCommands = {
     ["include"] = function(filePath, currentFilename) --{{#include filepath}}
